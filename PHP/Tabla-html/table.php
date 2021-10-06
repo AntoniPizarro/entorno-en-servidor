d7abd0c317c9ca5862709d5a9504c0d5934ee38c
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-10-06 20:17:48 +0200

    feat: Ejercicio PHP
    Funciones cambiadas para comprobar el open / close

--- PHP/Tabla-html/table.php
+++ PHP/Tabla-html/table.php
@@ -4,22 +4,38 @@
       border: 1px solid black;
     }
     </style>";
-    $rows = array("Toni", "Laura", "Pere","Pau");
+
+    $rows = array("Toni", "Laura", "Pere", null);
     $columns = "<tr>
                     <th>Nombre</th>
                     <th>empty()</th>
-                    <th>strlen()</th>
+                    <th>isset()</th>
+                    <th>boolval()</th>
                 </tr>";
     $table = "<table>".$columns;
+    
     foreach($rows as $name) {
+        $col2 = empty($name);
+        $col3 = isset($name);
+        $col4 = boolval($name);
+
+        if ($name == null) {
+            $name = "null";
+            $col2 = empty(null);
+            $col3 = 0;
+            $col4 = boolval(null);
+        }
+
         $row = "<tr>
                     <th>$name</th>
-                    <th>".empty($name)."</th>
-                    <th>".strlen($name)."</th>
+                    <th>".$col2."</th>
+                    <th>".$col3."</th>
+                    <th>".$col4."</th>
                 </tr>";
         $table = $table.$row;
     }
     $table = $table."</table>";
+
     echo $styles;
     echo $table;
 ?>
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # entorno-en-servidor
 Códigos de la asignatura
+
+
+## Iniciar servidor PHP:
+
+- CMD: php -S 127.0.0.1:3000
