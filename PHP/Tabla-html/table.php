33fb724bcdf578de059e067a14487a0cfc9cb5cf
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-11-28 21:18:24 +0100

    tabla reconstruida

--- PHP/Tabla-html/table.php
+++ PHP/Tabla-html/table.php
@@ -1,11 +1,7 @@
 <?php
     $styles = '<link rel="stylesheet" type="text/css" href="styles.css">';
 
-<<<<<<< HEAD
     $var = 1234;
-=======
-    // Declaración los datos por cada fila
->>>>>>> c086f8b8e1789e78690a3d464a7fd20f86b05653
     $rows = array(
         "",
         null,
@@ -13,20 +9,17 @@
         array(),
         array(1, 2, 3),
         false,
-        true,
-        1,
-        42,
-        0,
-        -1,
-        "1",
-        "0",
-        "1",
-        "php",
-        "true",
         "false"
     );
 
-<<<<<<< HEAD
+    $columns = "<tr>
+                    <th>Valor</th>
+                    <th>gettype()</th>
+                    <th>empty()</th>
+                    <th>is_null()</th>
+                    <th>isset()</th>
+                    <th>bool</th>
+                </tr>";
     $columns = '<tr class="headers">
     <th>Valor</th>
     <th>gettype()</th>
@@ -37,41 +30,27 @@
 </tr>';
 
     $table = "<table>".$columns;
-    
-    foreach ($rows as $name) {
-=======
-    // Declaración de cabeceras
-    $columns = "<tr>
-                    <th>Valor</th>
-                    <th>gettype()</th>
-                    <th>empty()</th>
-                    <th>is_null()</th>
-                    <th>isset()</th>
-                    <th>bool</th>
-                </tr>";
 
-    $table = "<table>".$columns;
-    
-    // Cálculo por cada dato por columna
-    foreach($rows as $name) {
->>>>>>> c086f8b8e1789e78690a3d464a7fd20f86b05653
+    foreach ($rows as $name) {
         $col2 = gettype($name);
         if ($col2 == "array") {
             $col1 = 'array('.implode(", ", $name).')';
         }else {
             $col1 = json_encode($name);
+            $col6 = json_encode(false);
         }
         $col3 = json_encode(empty($name));
         $col4 = json_encode(is_null($name));
         $col5 = json_encode(isset($name));
 
-        if ($name) {
-            $col6 = json_encode(true);
-        }else {
-            $col6 = json_encode(false);
-        }
-<<<<<<< HEAD
-
+        $row = '<tr>
+                    <th>$x = '.$col1.'</th>
+                    <th>'.$col2.'</th>
+                    <th>'.$col3.'</th>
+                    <th>'.$col4.'</th>
+                    <th>'.$col5.'</th>
+                    <th>'.$col6.'</th>
+                </tr>';
         if ($col3 == 'true') {
             $sty3 = 'style="background-color: greenyellow;"';
         } else {
@@ -97,29 +76,15 @@
         }
 
         $row = "<tr>
-    <th>".'$x'." = ".$col1."</th>
-    <th>".$col2."</th>
-    <th $sty3>$col3</th>
-    <th $sty4>$col4</th>
-    <th $sty5>$col5</th>
-    <th $sty6>$col6</th>
-</tr>";
-=======
-        
-        // Inserción de datos en tabla
-        $row = '<tr>
-                    <th>$x = '.$col1.'</th>
-                    <th>'.$col2.'</th>
-                    <th>'.$col3.'</th>
-                    <th>'.$col4.'</th>
-                    <th>'.$col5.'</th>
-                    <th>'.$col6.'</th>
-                </tr>';
->>>>>>> c086f8b8e1789e78690a3d464a7fd20f86b05653
+        <th>".'$x'." = ".$col1."</th>
+        <th>".$col2."</th>
+        <th $sty3>$col3</th>
+        <th $sty4>$col4</th>
+        <th $sty5>$col5</th>
+        <th $sty6>$col6</th></tr>";
         $table = $table.$row;
     }
     $table = $table."</table>";
-
     echo $styles;
     echo $table;
 ?>
